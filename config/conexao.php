abc38834635f2681e97229c6901dde6a463b3893
Julia <Carvalho.ju1233@gmail.com>
2026-04-09 14:48:33 -0300

    09.04

--- config/conexao.php
+++ config/conexao.php
@@ -6,8 +6,11 @@ $pass = "123456";
 
 try{
      $pdo = new PDO("mysql:host=$host;dbname =$db;charset=utf8",$user,$pass);
+     $pdo -> setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+     echo "conectado com sucesso <br>";
+     var_dump($pdo);
 }catch(PDOException $e){
-    die("Erro na conexão: ".$e->getMessage());
+    var_dump($e ->getMessage());
+    // die("Erro na conexão: ".$e->getMessage());
 }
-    
 
