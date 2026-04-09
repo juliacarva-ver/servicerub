1456efc8f7e5b8afe6976efd3c425d96a6562f98
Julia <Carvalho.ju1233@gmail.com>
2026-04-09 15:45:38 -0300

    feito

--- aulapdo.php
+++ aulapdo.php
@@ -1,3 +1,39 @@
 <?php
-include_once "config/conexap.php"
+include_once "config/conexao.php";
+ 
+$sql = "select * from usuarios";
+$cmd = $pdo->prepare($sql);
+$cmd->execute();
+ 
+$servicos = $cmd->fetchAll(PDO::FETCH_ASSOC);
+ var_dump($servicos)
 ?>
+ 
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <title>Aula PDO PHP</title>
+</head>
+<body>
+    <h2>Lista de Seriços</h2>
+    <table border="1" cellpadding = 10>
+        <tr>
+            <th>ID</th>
+            <th>Nome</th>
+            <th>Descrição</th>
+            <th>Preço</th>
+            <th>Descontinuado</th>
+        </tr>
+ 
+        <tr>
+            <td>1</td>
+            <td>Formatacao</td>
+            <td>Vamos formatar</td>
+            <td>R$150,00</td>
+            <td>Não</td>
+        </tr>
+    </table>
+</body>
+</html>
+
--- config/conexao.php
+++ config/conexao.php
@@ -6,11 +6,13 @@ $pass = "123456";
 
 try{
      $pdo = new PDO("mysql:host=$host;dbname =$db;charset=utf8",$user,$pass);
-     $pdo -> setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
-     echo "conectado com sucesso <br>";
-     var_dump($pdo);
+     $pdo ->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+    //  echo "conectado com sucesso <br>";
+    //  var_dump($pdo);
+
+
 }catch(PDOException $e){
-    var_dump($e ->getMessage());
-    // die("Erro na conexão: ".$e->getMessage());
+    // var_dump($e ->getMessage());
+    die("Erro na conexão: ".$e->getMessage());
 }
 
