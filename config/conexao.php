f9a4c8278b27330dca11742de884aeb475a2ad81
Julia <Carvalho.ju1233@gmail.com>
2026-04-14 15:30:31 -0300

    14

--- config/conexao.php
+++ config/conexao.php
@@ -1,8 +1,16 @@
 <?php
+
+
+function obterPdo():PDO{
+
+
 $host ="10.91.47.46";
 $db = "servicehubdb01";
 $user = "root";
 $pass = "123456";
+
+static $pdo;
+
 
 try{
      $pdo = new PDO("mysql:host=$host;dbname =$db;charset=utf8",$user,$pass);
@@ -14,5 +22,10 @@ try{
 }catch(PDOException $e){
     // var_dump($e ->getMessage());
     die("Erro na conexão: ".$e->getMessage());
+}  
+return $pdo;
 }
+
+
+
 
