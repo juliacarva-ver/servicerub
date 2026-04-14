b4cb5b9464874b6b1f8f13ffe91d976cc5af14a9
Julia <Carvalho.ju1233@gmail.com>
2026-04-14 16:29:28 -0300

    fazer

---
+++ login.php
@@ -0,0 +1,46 @@
+<<?php 
+require "class/Usuario.php";
+$user = new Usuario();
+// var_dump($user->efetuarLogin('admin@servicehub.com', 'admin123'));
+$msg = "";
+if ($_SERVER['REQUEST_METHOD']==="POST"){
+    $email = filter_input(INPUT_POST, "email", FILTER_VALIDATE_EMAIL);
+    $senha = $_POST["senha"]?? null;
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+  <title>Login</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+
+<body class="bg-light">
+<div class="container mt-5">
+  <div class="card shadow p-4 col-md-5 mx-auto">
+    <h3 class="text-center">Área Restrita</h3>
+
+
+    <form method="POST">
+      <div class="mb-3">
+        <label>Email</label>
+        <input type="email" name="email" class="form-control" required>
+      </div>
+
+      <div class="mb-3">
+        <label>Senha</label>
+        <input type="password" name="senha" class="form-control" required>
+      </div>
+
+      <button class="btn btn-dark w-100">Entrar</button>
+    </form>
+
+    <p class="text-center mt-3">
+      <a href="index.php">Voltar ao site</a>
+    </p>
+  </div>
+</div>
+</body>
